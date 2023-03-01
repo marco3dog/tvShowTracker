fb19e65a2379283111e827ff01fcb2ec49214e44
Gavin Liburd <GavinLiburd901@gmail.com>
2023-03-01 14:58:50 -0500

    Updated User class

--- TVTracker/src/com/cognixia/jump/tvtracker/User.java
+++ TVTracker/src/com/cognixia/jump/tvtracker/User.java
@@ -2,20 +2,38 @@ package com.cognixia.jump.tvtracker;
 
 import java.util.List;
 
+package com.cognixia.jump.tvtracker;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 	
 	private int id;
 	private String name;
 	private String password;
-	private List<Show> list;
+	private List<UserShow> list;
+	private Connection conn;
 	
 	
-	public User(int id, String name, String password, List<Show> list) {
+	public User(int id, String name, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.password = password;
-		this.list = list;
+		this.list = new ArrayList<UserShow>();
+		
+		try{
+			this.conn = BetterConnectionManager.getConnection();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		createList();
 	}
 	
 	public int getId() {
@@ -36,13 +54,31 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public List<Show> getList() {
+	public List<UserShow> getList() {
 		return list;
 	}
-	public void setList(List<Show> list) {
+	public void setList(List<UserShow> list) {
 		this.list = list;
 	}
-
+	
+	private void createList(){
+		//List<UserShow> usersShows = new ArrayList<UserShow>(); 
+		try (Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT s.showid, s.name, us.episodes, s.episodes FROM user_shows us JOIN user u ON us.userid = u.userid JOIN shows s ON us.showid = s.showid WHERE us.userid = " + this.id);
+				) {
+				while (rs.next()) {
+					int id = rs.getInt("showid");
+					String name = rs.getString("name");
+					int episodesWatched = rs.getInt("us.episodes");
+					int episodesTotal = rs.getInt("s.episodes");
+					UserShow show = new UserShow(id, name, episodesTotal, episodesWatched);
+					list.add(show);
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", password=" + password + ", list=" + list + "]";
