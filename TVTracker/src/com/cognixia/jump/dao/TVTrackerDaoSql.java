9f553bb14d44a346feb9a850eae53f030377fa95
Gavin Liburd <GavinLiburd901@gmail.com>
2023-03-01 14:48:17 -0500

    Created the updateShow, deleteShow, and UpdateShow Dao methods

--- TVTracker/src/com/cognixia/jump/dao/TVTrackerDaoSql.java
+++ TVTracker/src/com/cognixia/jump/dao/TVTrackerDaoSql.java
@@ -3,6 +3,7 @@ package com.cognixia.jump.dao;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -31,11 +32,11 @@ public class TVTrackerDaoSql implements TVTrackerDao {
 			ResultSet rs = stmt.executeQuery("SELECT * FROM shows");
 			) {
 			while (rs.next()) {
-				int id = rs.getInt("showid");
+				int showId = rs.getInt("showid");
 				String name = rs.getString("name");
 				int episodes = rs.getInt("episodes");
 				
-				Show show = new Show(id, name, episodes);
+				Show show = new Show(showId, name, episodes);
 				showList.add(show);
 			}
 		} catch (SQLException e) {
@@ -48,25 +49,84 @@ public class TVTrackerDaoSql implements TVTrackerDao {
 
 	@Override
 	public Optional<Show> getShowById(int id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		
+		try (PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM show WHERE showid = ?");) {
+			pstmt.setInt(1, id);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				int showId = rs.getInt("showid");
+				String name = rs.getString("name");
+				int episodes = rs.getInt("episodes");
+				
+				Show show = new Show(showId, name, episodes);
+				Optional<Show> foundShow = Optional.of(show);
+				rs.close();
+				return foundShow;
+			} else {
+				rs.close();
+				return Optional.empty();
+			}
+		} catch (SQLException e) {
+			return Optional.empty();
+		}
 	}
 
 	@Override
-	public boolean createShow(Show show) {
-		// TODO Auto-generated method stub
+	public boolean createShow(String showName, int episodes) {
+		
+		try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO show values(null, name = ?, episodes = ?)");) {
+			
+			pstmt.setString(1, showName);
+			pstmt.setInt(2, episodes);
+			
+			int updated = pstmt.executeUpdate();
+			
+			if (updated == 1)
+				return true;
+			
+		} catch (SQLException e) {
+			return false;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean deleteShow(int id) {
-		// TODO Auto-generated method stub
+		
+		try (PreparedStatement pstmt = conn.prepareStatement("DELETE FROM show WHERE showid = ?");) {
+			
+			pstmt.setInt(1, id);
+			
+			int updated = pstmt.executeUpdate();
+			
+			if (updated == 1)
+				return true;
+			
+		} catch (SQLException e) {
+			return false;
+		}
 		return false;
 	}
 
 	@Override
-	public boolean updateShow(Show show) {
-		// TODO Auto-generated method stub
+	public boolean updateShow(String showName, int episodes) {
+		
+		
+		try (PreparedStatement pstmt = conn.prepareStatement("UPDATE show SET name = ?, episodes = ?");) {
+			
+			pstmt.setString(1, showName);
+			pstmt.setInt(2, episodes);
+			
+			int updated = pstmt.executeUpdate();
+			
+			if (updated == 1)
+				return true;
+			
+		} catch (SQLException e) {
+			return false;
+		}
 		return false;
 	}
 	
